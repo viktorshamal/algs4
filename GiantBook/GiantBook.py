b64a21bbc3a375bbc5600c2765f81c53a371e589
Viktor Shamal Andersen <sphinxominator@gmail.com>
2018-02-08 14:55:08 +0100

    GiantBook working with giant component

--- GiantBook/GiantBook.py
+++ GiantBook/GiantBook.py
@@ -1,3 +1,41 @@
 """A client that searches for giant components in a network"""
+from MyUnionFind import MyUnionFind
+from random import randint
 
-print("Hello world")
+
+class GiantBook:
+    def __init__(self, count):
+        self.count = count
+        self.store = MyUnionFind(count)
+        self.giantComponent = False
+
+    def checkGiantComponent(self, i):
+        n_half = int(self.count * 0.5)
+
+        if not self.giantComponent and self.store.maxComponentSize > n_half:
+            self.giantComponent = i
+
+
+w
+    def simulateRandomConnections(self):
+        i = 0
+
+        while True:
+            p = randint(0, self.count - 1)
+            q = randint(0, self.count - 1)
+
+            if not self.store.connected(p, q):
+                self.store.union(p, q)
+
+            self.checkGiantComponent(i)
+
+            i += 1
+
+            if self.store.count() == 1:
+                break
+
+        print(self.count, self.giantComponent)
+
+
+g = GiantBook(int(1e5))
+g.simulateRandomConnections()
--- GiantBook/MyUnionFind.py
+++ GiantBook/MyUnionFind.py
@@ -1,1 +1,35 @@
 """A Union-Find data structure"""
+from algs4.fundamentals.uf import WeightedQuickUnionUF as UF
+
+
+class MyUnionFind(UF):
+    def __init__(self, *args, **kwargs):
+        super(MyUnionFind, self).__init__(*args, **kwargs)
+        self.maxComponentSize = 1
+
+    def union(self, p, q):
+        """
+        Merges the component containing site p with the
+        component containing site q.
+
+        :param p: the integer representing one site
+        :param q: the integer representing the other site
+        """
+        root_p = self.find(p)
+        root_q = self.find(q)
+        if root_p == root_q:
+            return
+
+        # make root of smaller rank point to root of larger rank
+        if self._size[root_p] < self._size[root_q]:
+            small, large = root_p, root_q
+        else:
+            small, large = root_q, root_p
+
+        self._parent[small] = large
+        self._size[large] += self._size[small]
+
+        if self._size[large] > self.maxComponentSize:
+            self.maxComponentSize = self._size[large]
+
+        self._count -= 1
