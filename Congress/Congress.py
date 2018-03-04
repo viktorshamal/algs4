9b303f4bc5eb9145de7ed3848c5b994da348eeb9
Viktor Shamal Andersen <sphinxominator@gmail.com>
2018-03-04 20:29:41 +0100

    No need to wrap congress in a function

--- Congress/Congress.py
+++ Congress/Congress.py
@@ -6,34 +6,31 @@ from sys import stdin
 def geometric_mean(n):
     return sqrt(n * (n+1))
 
-def apport():
-    heap = []
-    
-    next(stdin) # skip over states
-    seats = int(stdin.readline())
+heap = []
 
-    State = namedtuple('State', ['pop','orgpop','seats','name'])
-    
-    while True:
-        name = stdin.readline().strip()
-        population = stdin.readline().strip()
+next(stdin) # skip over states
+seats = int(stdin.readline())
 
-        if population:
-            orgpop = -1 * int(population)
-            pop = orgpop / geometric_mean(1)
-            entry = State(pop, orgpop, 1, name)
-            heapq.heappush(heap, entry)
-        else:
-            break
+State = namedtuple('State', ['pop','orgpop','seats','name'])
 
-    for _ in range(seats - len(heap)):
-        largest = heap[0]
-        new_seats = largest.seats + 1
-        reduced_pop = largest.orgpop / geometric_mean(new_seats)
-        new_state = State(reduced_pop, largest.orgpop, new_seats, largest.name)
-        heapq.heapreplace(heap, new_state)
+while True:
+    name = stdin.readline().strip()
+    population = stdin.readline().strip()
 
-    for state in heap:
-        print(state.name, state.seats)
+    if population:
+        orgpop = -1 * int(population)
+        pop = orgpop / geometric_mean(1)
+        entry = State(pop, orgpop, 1, name)
+        heapq.heappush(heap, entry)
+    else:
+        break
 
-apport()
+for _ in range(seats - len(heap)):
+    largest = heap[0]
+    new_seats = largest.seats + 1
+    reduced_pop = largest.orgpop / geometric_mean(new_seats)
+    new_state = State(reduced_pop, largest.orgpop, new_seats, largest.name)
+    heapq.heapreplace(heap, new_state)
+
+for state in heap:
+    print(state.name, state.seats)
